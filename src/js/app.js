69a73ce3fd73dd63ae922d69ada60b61f6f1817e
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-14 02:00:41 -0400

    create if statement check if propertyName includes 'flex' then apply 'open-active' class

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,8 @@ function toggleOpen() {
 
 function toggleActive(e){
     console.log(e.propertyName);
-    
+    if(e.propertyName.includes('flex'))
+    this.classList('open-active')
 };
 
 panels.forEach((panel) => panel.addEventListener("click", toggleOpen));

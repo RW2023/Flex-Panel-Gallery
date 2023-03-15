d2590625390cc8d580c30953eae565e634052ab0
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-15 01:03:45 -0400

    Add highlight letter function also remove space from html and remove console logs

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,39 @@
 const panels = document.querySelectorAll(".panel");
 
+function highlightLetters() {
+  panels.forEach((panel) => {
+    const words = panel.querySelectorAll("p");
+    words.forEach((word) => {
+      const letters = word.textContent.split("");
+      word.innerHTML = "";
+      letters.forEach((letter) => {
+        const span = document.createElement("span");
+        span.textContent = letter;
+        word.appendChild(span);
+      });
+    });
+
+    const spans = panel.querySelectorAll("span");
+    spans.forEach((span, index) => {
+      const transformY = getComputedStyle(span).transform.split(",")[5].trim();
+      const height = span.getBoundingClientRect().height;
+      const offset =
+        transformY === "none" ? height * index : height * index - transformY;
+      const hue = (offset / panel.offsetHeight) * 360;
+      span.style.color = `hsl(${hue}, 100%, 50%)`;
+    });
+  });
+}
+
 function toggleOpen() {
-  console.log("Hello");
   this.classList.toggle("open");
+  highlightLetters();
 }
 
 function toggleActive(e) {
-  console.log(e.propertyName);
   if (e.propertyName.includes("flex")) {
     this.classList.toggle("open-active");
+    highlightLetters();
   }
 }
 
